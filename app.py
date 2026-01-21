661d8e85c1ba2113aee114f057658839fbebb6d4
spirit-dave <adeyemi.b.david@gmail.com>
2026-01-21 18:46:10 +0000

    .

--- app.py
+++ app.py
@@ -127,5 +127,8 @@ def chat():
         "message": response
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
